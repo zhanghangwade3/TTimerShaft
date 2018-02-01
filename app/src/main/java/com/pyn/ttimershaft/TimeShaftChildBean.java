6650b49eb4a1165ce8efa36da39fcf82d852406c
zhanghangwade3 <zhanghangwade@126.com>
2018-02-02 00:46:43 +0800

    Initial commit

---
+++ app/src/main/java/com/pyn/ttimershaft/TimeShaftChildBean.java
@@ -0,0 +1,33 @@
+package com.pyn.ttimershaft;
+
+
+public class TimeShaftChildBean {
+
+    private String _id;     // id
+    private String title;   // 标题
+    private String content; // 内容
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/pyn/ttimershaft/TimeShaftParentBean.java
@@ -0,0 +1,24 @@
+package com.pyn.ttimershaft;
+
+
+public class TimeShaftParentBean {
+
+    private String time;
+    private String data;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
